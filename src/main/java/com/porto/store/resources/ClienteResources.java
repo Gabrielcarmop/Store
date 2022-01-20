a16f8713f49d33e5cdbfc3de5a231e70701af605
Gabriel Porto <38323680+Gabrielcarmop@users.noreply.github.com>
2022-01-19 22:26:35 -0300

    Finalização do CRUD

--- src/main/java/com/porto/store/resources/ClienteResources.java
+++ src/main/java/com/porto/store/resources/ClienteResources.java
@@ -1,14 +1,25 @@
 package com.porto.store.resources;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.porto.store.domain.Cliente;
+import com.porto.store.dtos.ClienteDto;
 import com.porto.store.service.ClienteService;
+
+import java.net.URI;
+import java.util.*;
 
 @RestController
 @RequestMapping(value="/Clientes")
@@ -21,5 +32,31 @@ public class ClienteResources {
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id){
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping 
+	public ResponseEntity<List<ClienteDto>> findAll(){
+		List<Cliente> list = service.findAll();
+		List<ClienteDto> listDto = list.stream().map(obj -> new ClienteDto(obj)).collect(Collectors.toList()); 
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> create(@RequestBody Cliente obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClienteDto> update(@PathVariable Integer id, @RequestBody ClienteDto objDto){
+		Cliente newObj = service.update(id,objDto);
+		return ResponseEntity.ok().body(new ClienteDto(newObj));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/porto/store/service/ClienteService.java
+++ src/main/java/com/porto/store/service/ClienteService.java
@@ -1,11 +1,13 @@
 package com.porto.store.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.porto.store.domain.Cliente;
+import com.porto.store.dtos.ClienteDto;
 import com.porto.store.exceptions.ObjectNotFoundException;
 import com.porto.store.repositories.ClienteRepository;
 
@@ -20,4 +22,25 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+id+", Tipo: "+ Cliente.class.getName()));
 	}
 	
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	public Cliente create(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public Cliente update(Integer id,ClienteDto objDto) {
+		Cliente obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setEndereco(objDto.getEndereco());
+		obj.setTelefone(objDto.getTelefone());
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
 }
